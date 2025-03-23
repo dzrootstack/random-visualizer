4d1fe048850c63170d9f944b08956d80b0de44c1
dzrootstack <dzrootstack@outlook.com>
2025-03-23 17:00:40 -0400

    Fix the build issue

--- src/components/sidebar/Code.tsx
+++ src/components/sidebar/Code.tsx
@@ -1,16 +1,18 @@
-import React, { createRef, useEffect, useState } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/prism";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function Code({ code }: { code: string }) {
-
   return (
-    <SyntaxHighlighter language="javascript" style={vscDarkPlus} customStyle={{
-      borderRadius: 8,
-      flexShrink: 0,
-    }}>
+    <SyntaxHighlighter
+      language="javascript"
+      style={vscDarkPlus}
+      customStyle={{
+        borderRadius: 8,
+        flexShrink: 0,
+      }}
+    >
       {code}
     </SyntaxHighlighter>
   );
 }
-
